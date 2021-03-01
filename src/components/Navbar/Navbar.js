2b7144cf875196c720fb597ad87a0bc8a9c362e8
Sachchaa <shewahal@deakin.edu.au>
2021-03-01 23:34:38 +1100

    React-website updated -
    Started working on the website
    Installed react-router-dom, react-icons
    Installed react-scroll and styled-components
    Created Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcons,
+    NavMenu,
+    NavItem,
+    NavLink,
+    NavButton,
+    NavButtonLink
+} from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>
+                        <h1>LOGO</h1>
+                    </NavLogo>
+                    <MobileIcons>
+                        <FaBars />
+                    </MobileIcons>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLink to='about'>About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='services'>Services</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='contact-us'>Contact Us</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='signup'>Sign Up</NavLink>
+                        </NavItem>
+                    </NavMenu>
+                    <NavButton>
+                        <NavButtonLink to='signin'>
+                            Sign In
+                        </NavButtonLink>
+                    </NavButton>
+
+                </NavbarContainer>
+            </Nav>
+
+        </>
+    )
+}
+
+export default Navbar
