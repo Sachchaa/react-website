828325c67faf33c28a348d922e515fec31ac8b77
Sachchaa <shewahal@deakin.edu.au>
2021-03-03 00:58:42 +1100

    React-website updated -
    Created hero component
    Added button element

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Button } from '../ButtonElement'
+import Video from '../../videos/video.mp4'
+
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroButtonWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements'
+
+const Hero = () => {
+
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Do you need a balloon ride?</HeroH1>
+                <HeroP>
+                    Sign up for a ride today.
+                </HeroP>
+                <HeroButtonWrapper>
+                    <Button to='sign-up' onMouseEnter={onHover} onMouseLeave={onHover}>
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroButtonWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default Hero
