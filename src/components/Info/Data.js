e2548958dcc0fcb7596ccd1c9cef17f5048ead25
Sachchaa <shewahal@deakin.edu.au>
2021-03-04 23:23:13 +1100

    React-website updated -
    Data component added

---
+++ src/components/Info/Data.js
@@ -0,0 +1,16 @@
+export const homeObject = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Balloons',
+    headLine: 'Need a Hot Air Balloon ride?',
+    description: 'Sign up for a booking',
+    buttonLable: 'Get Started',
+    imgStart: false,
+    //img: ,
+    alt: 'car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
