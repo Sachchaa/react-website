ccf53f23fa24982435c0eb777cd96fb299c7bfcf
Sachchaa <shewahal@deakin.edu.au>
2021-03-07 23:50:20 +1100

    React-website updated -
    Created sign in page
    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Home from './pages';
+import SignIn from './pages/signin';
 
 const App = () => {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/signin' component={SignIn} />
+      </Switch>
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink
+
+} from './FooterElements'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>
+                            </FooterLinkTitle>
+                            <FooterLink>About Us</FooterLink>
+                            <FooterLink>How it works</FooterLink>
+                            <FooterLink>Careers</FooterLink>
+                            <FooterLink>Investors</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
